cd66bebff8f2829faa6ccc29fdbd6de8210630fb
delmas007 <azertyazerty1ze@gmail.com>
2023-11-27 12:50:32 +0000

    Model

--- APPARTE/settings.py
+++ APPARTE/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'APPARTE.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'don',
+        'USER': 'postgres',
+        'PASSWORD': 'delmas',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

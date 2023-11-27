a12aea36b9425757a549109fe80009f0ace66b12
delmas007 <azertyazerty1ze@gmail.com>
2023-11-27 13:26:22 +0000

    Model

--- Model/models.py
+++ Model/models.py
@@ -1,3 +1,60 @@
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from django.db import models
 
 # Create your models here.
+sex = (
+    ('Homme', 'Homme'),
+    ("Femme", "Femme"),
+)
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Vous devez entrer un mail")
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email=email, password=password)
+        user.is_staff = True
+        user.is_admin = True
+        user.save()
+        return user
+
+
+class Utilisateur(AbstractBaseUser):
+    email = models.EmailField(
+        unique=True,
+        max_length=255,
+        blank=False
+    )
+    nom = models.CharField(max_length=250, verbose_name='nom')
+    prenom = models.CharField(max_length=250)
+    contact = models.IntegerField(max_length=10)
+    commune = models.CharField(max_length=250)
+    sexe = models.CharField(blank=False, choices=sex)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    objects = MyUserManager()
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    class Meta:
+        verbose_name = 'utilisateur'
+
+    def __str__(self):
+        return self.nom

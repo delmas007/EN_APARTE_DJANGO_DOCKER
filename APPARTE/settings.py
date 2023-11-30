ee147fd3f1ca1bb5344921e3433a2fc48fb9fa1f
delmas007 <azertyazerty1ze@gmail.com>
2023-11-30 12:57:46 +0000

    employer

--- APPARTE/settings.py
+++ APPARTE/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Model',
-    'Vitrine'
+    'Vitrine',
+    'Admin',
+    'Employer'
 ]
 
 MIDDLEWARE = [

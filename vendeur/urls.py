df40152c8913c93e86bbaa33ae32fc0f402edfd3
delmas007 <azertyazerty1ze@gmail.com>
2023-12-06 15:46:19 +0000

    vendeur commande

--- vendeur/urls.py
+++ vendeur/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from vendeur.views import ajouter_produit
+from vendeur.views import ajouter_produit, liste_produits, modifier_produit, supprimer_produit
 
 app_name = 'vendeur'
 urlpatterns = [
     path('ajouter_un_produit/', ajouter_produit, name='ajouter_un_produit'),
+    path('liste_produits/', liste_produits, name='liste_produits'),
+    path('modifier_produit/<int:produit_id>/', modifier_produit, name='modifier_produit'),
+    path('produits/<int:produit_id>/supprimer/', supprimer_produit, name='supprimer_produit'),
 ]
--- vendeur/views.py
+++ vendeur/views.py
@@ -1,9 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.csrf import csrf_protect
 
-from Model.models import Roles
+from Model.models import Roles, Produit
 from vendeur.forms import UserRegistrationFormee
 
 
@@ -25,3 +25,38 @@ def ajouter_produit(request):
     form = UserRegistrationFormee()
     context['form'] = form
     return render(request, 'produit_A.html', context=context)
+
+
+@login_required
+def liste_produits(request):
+    if not request.user.roles or request.user.roles.role != 'VENDEUR':
+        return redirect('Accueil')
+    produits = Produit.objects.all()
+    return render(request, 'list_PO.html', {'produits': produits})
+
+
+@login_required
+def modifier_produit(request, produit_id):
+    if not request.user.roles or request.user.roles.role != 'VENDEUR':
+        return redirect('Accueil')
+    produit = get_object_or_404(Produit, id=produit_id)
+
+    if request.method == 'POST':
+        form = UserRegistrationFormee(request.POST, request.FILES, instance=produit)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Produit modifier avec succès !')
+    else:
+        form = UserRegistrationFormee(instance=produit)
+
+    return render(request, 'produit_M.html', {'form': form, 'produit': produit})
+
+
+@login_required
+def supprimer_produit(request, produit_id):
+    if not request.user.roles or request.user.roles.role != 'VENDEUR':
+        return redirect('Accueil')
+    produit = get_object_or_404(Produit, id=produit_id)
+    produit.delete()
+    messages.success(request, 'Produit supprimé avec succès !')
+    return redirect('vendeur:liste_produits')

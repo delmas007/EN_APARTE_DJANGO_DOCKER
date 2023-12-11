93955ac531d273a6515f787334bef227209931cd
delmas007 <azertyazerty1ze@gmail.com>
2023-12-11 17:15:25 +0000

    url

--- Vitrine/urls.py
+++ Vitrine/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 
-from .views import Accueil, Contact, Faq,Produit,About,Produitdetails
+from .views import Accueil, Contact, Faq, Produit, About, liste_tous_produits, produit_details, \
+    ajouter_panier, commander_produit
 
 app_name = 'vitrine'
 
 urlpatterns = [
-
-    path('Contact/', Contact, name='Contact'),
-    path('Faq/', Faq, name='Faq'),
     path('Accueil/', Accueil, name='Accueil'),
     path('A propos/', About, name='About'),
-    path('Produit/', Produit, name='Produit'),
-    path('Produit details/', Produitdetails, name='Produit details'),
+    path('liste_tous_produits/', liste_tous_produits, name='liste_tous_produits'),
+    -    path('produit/<int:produit_id>/', produit_details, name='Produit details'),
+    +    path('produit/<int:produit_id>/', produit_details, name='Produit_details'),
+    +    path('produits/<int:produit_id>/ajouter', ajouter_panier, name='panier'),
+    path('commander_produit/', commander_produit, name='commander_produit'),
 ]
+

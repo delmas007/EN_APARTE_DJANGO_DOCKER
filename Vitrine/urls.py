e58a8fa7fba0077159da0e45b4dcddefaec38754
delmas007 <azertyazerty1ze@gmail.com>
2023-12-11 17:16:55 +0000

    url

--- Vitrine/urls.py
+++ Vitrine/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('Accueil/', Accueil, name='Accueil'),
     path('A propos/', About, name='About'),
     path('liste_tous_produits/', liste_tous_produits, name='liste_tous_produits'),
-    -    path('produit/<int:produit_id>/', produit_details, name='Produit details'),
-    +    path('produit/<int:produit_id>/', produit_details, name='Produit_details'),
-    +    path('produits/<int:produit_id>/ajouter', ajouter_panier, name='panier'),
+    path('produit/<int:produit_id>/', produit_details, name='Produit details'),
+    path('produit/<int:produit_id>/', produit_details, name='Produit_details'),
+    path('produits/<int:produit_id>/ajouter', ajouter_panier, name='panier'),
     path('commander_produit/', commander_produit, name='commander_produit'),
 ]
 

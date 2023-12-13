b2bc5d713a7afcee29e80636a508d158d0284a96
delmas007 <azertyazerty1ze@gmail.com>
2023-12-13 17:05:20 +0000

    panier

--- Vitrine/urls.py
+++ Vitrine/urls.py
@@ -7,9 +7,9 @@ app_name = 'vitrine'
 
 urlpatterns = [
     path('Accueil/', Accueil, name='Accueil'),
-    path('A propos/', About, name='About'),
-    path('A propos/', Faq, name='Faq'),
-    path('A propos/', Contact, name='Contact'),
+    path('About/', About, name='About'),
+    path('Faq/', Faq, name='Faq'),
+    path('Contact/', Contact, name='Contact'),
     path('liste_tous_produits/', liste_tous_produits, name='liste_tous_produits'),
     path('produit/<int:produit_id>/', produit_details, name='Produit_details'),
     path('produits/<int:produit_id>/ajouter', ajouter_panier, name='panier'),

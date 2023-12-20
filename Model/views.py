82486ad569ca8f38dba5fcf6805b6f7754060d38
delmas007 <azertyazerty1ze@gmail.com>
2023-12-20 14:23:35 +0000

    horaire & service

--- Model/views.py
+++ Model/views.py
@@ -1,10 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_protect
-
 from Model.forms import ConnexionForm, UserRegistrationForm, RendezVousForm
 from Model.models import Roles, Service
+from django.contrib import messages
+
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.core.mail import EmailMessage
+
+from Model.tokens import account_activation_token
 
 
 # Create your views here.
@@ -24,6 +33,43 @@ class Connexion(LoginView):
             return reverse('vendeur:ajouter_un_produit')
 
 
+def activate(request, uidb64, token):
+    User = get_user_model()
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except:
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+
+        messages.success(request, "Thank you for your email confirmation. Now you can login your account.")
+        return redirect('login')
+    else:
+        messages.error(request, "Activation link is invalid!")
+
+    return redirect('homepage')
+
+
+def activateEmail(request, user, to_email):
+    mail_subject = "Activate your user account."
+    message = render_to_string("template_activate_account.html", {
+        'user': user.username,
+        'domain': get_current_site(request).domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': account_activation_token.make_token(user),
+        "protocol": 'https' if request.is_secure() else 'http'
+    })
+    email = EmailMessage(mail_subject, message, to=[to_email])
+    if email.send():
+        messages.success(request, f'Dear <b>{user}</b>, please go to you email <b>{to_email}</b> inbox and click on \
+                received activation link to confirm and complete the registration. <b>Note:</b> Check your spam folder.')
+    else:
+        messages.error(request, f'Problem sending email to {to_email}, check if you typed it correctly.')
+
+
 @csrf_protect
 def inscription(request):
     context = {}
@@ -31,10 +77,10 @@ def inscription(request):
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-
             client_role = Roles.objects.get(role=Roles.CLIENT)
             user.roles = client_role
-
+            user.is_active = False
+            activateEmail(request, user, form.cleaned_data.get('email'))
             user.save()
             return redirect('Model:connexion')
         else:
